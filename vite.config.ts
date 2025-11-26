28e72b334e398bdc267c9c9ae778c70ef313b0bd
riceball <42161586+riceball-tw@users.noreply.github.com>
2025-11-26 22:30:35 +0800

    chore: Build gh-page demo site

--- vite.config.ts
+++ vite.config.ts
@@ -2,25 +2,35 @@ import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 import { resolve } from 'path'
 
-export default defineConfig({
-  base: '/view-transition-highlight/',
-  plugins: [
-    dts({ 
-      rollupTypes: true,
-      tsconfigPath: './tsconfig.json'
-    })
-  ],
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/utils/Highlighter.ts'),
-      name: 'ViewTransitionHighlight',
-      fileName: 'view-transition-highlight',
-    },
-    rollupOptions: {
-      // Ensure external dependencies are not bundled into your library
-      external: [], 
-      output: {
-        globals: {}
+export default defineConfig(({ mode }) => {
+  if (mode === 'demo') {
+    return {
+      base: '/view-transition-highlight/',
+      build: {
+        outDir: 'dist-demo'
+      }
+    }
+  }
+
+  return {
+    plugins: [
+      dts({ 
+        rollupTypes: true,
+        tsconfigPath: './tsconfig.json'
+      })
+    ],
+    build: {
+      lib: {
+        entry: resolve(__dirname, 'src/utils/Highlighter.ts'),
+        name: 'ViewTransitionHighlight',
+        fileName: 'view-transition-highlight',
+      },
+      rollupOptions: {
+        // Ensure external dependencies are not bundled into your library
+        external: [], 
+        output: {
+          globals: {}
+        }
       }
     }
   }
